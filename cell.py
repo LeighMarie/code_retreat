3cb893372a34cc8a725163e3ba623f0f3fe2fca6
Leigh Braswell <Lemur@Leighs-MacBook-Pro.local>
2016-08-05 13:56:58 -0700

    yay

--- cell.py
+++ cell.py
@@ -1,11 +1,19 @@
 
-
+import unittest
         
+class Game:
+	def __init__(self):
+		self.grid = {}
 
+	def set(self, x, y):
+		self.grid[(x, y)] = True
                 
 
 class ConwayTest(unittest.TestCase):
-
+	def testGetSet(self):
+		c = Game()
+		c.set(1, 1)
+		self.assertTrue(c.grid[(1, 1)])
                 
         
 
