2ad5f5f8b6dc532009ed5b4c9ab492492dadd38c
Leigh Braswell <Lemur@Leighs-MacBook-Pro.local>
2016-08-05 14:10:09 -0700

    check method done

--- cell.py
+++ cell.py
@@ -2,28 +2,42 @@
 import unittest
         
 class Game:
-	def __init__(self):
+	def __init__(self, size):
 		self.grid = {}
+		self.size = size
 
 	def set(self, x, y):
+		self.check(x)
+		self.check(y)
 		self.grid[(x, y)] = True
 
 	def get(self,x,y):
+		self.check(x)
+		self.check(y)
 		if (x,y) in self.grid:
 			return self.grid[(x,y)]
 		else:
 			return False
-                
+    
+	def check(self, index):
+		if index > self.size:
+			raise ValueError('Bad index')
 
 class ConwayTest(unittest.TestCase):
 	def testGetSet(self):
-		c = Game()
+		c = Game(5)
 		c.set(1, 1)
 		self.assertTrue(c.grid[(1, 1)])
 	def testGet(self):
-		c = Game()
+		c = Game(5)
 		c.set(2,2)
 		self.assertTrue(c.get(2,2))
+
+	def testCheck(self):
+		c = Game(5)
+		with self.assertRaises(ValueError):
+			c.set(6,6)
+
                 
         
 
